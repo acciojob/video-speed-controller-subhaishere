746ddd1e030f987990574ce9e335d6be55743887
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:50:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.player__video');
+const playButton = document.querySelector('.player__button.toggle');
+const volumeSlider = document.querySelector('.player__slider[name="volume"]');
+const playbackSpeedSlider = document.querySelector('.player__slider[name="playbackRate"]');
+const progressFilled = document.querySelector('.progress__filled');
+const skipButtons = document.querySelectorAll('.player__button[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Function to toggle play/pause when the play button is clicked
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// Function to update the play button appearance based on video playing status
+function updatePlayButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  playButton.textContent = icon;
+}
+
+// Function to update the volume of the video based on the volume slider value
+function updateVolume() {
+  video.volume = volumeSlider.value;
+}
+
+// Function to update the playback speed of the video based on the playback speed slider value
+function updatePlaybackSpeed() {
+  video.playbackRate = playbackSpeedSlider.value;
+}
+
+// Function to skip forward or backward in the video based on the data-skip value of the button
+function skipVideo() {
+  const skipValue = parseFloat(this.dataset.skip);
+  video.currentTime += skipValue;
+}
+
+// Function to update the progress bar as the video plays
+function updateProgressBar() {
+  const progress = (video.currentTime / video.duration) * 100;
+  progressFilled.style.flexBasis = `${progress}%`;
+}
+
+// Event listener for play button
+playButton.addEventListener('click', togglePlay);
+
+// Event listener for video play/pause
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+
+// Event listener for volume slider
+volumeSlider.addEventListener('input', updateVolume);
+
+// Event listener for playback speed slider
+playbackSpeedSlider.addEventListener('input', updatePlaybackSpeed);
+
+// Event listeners for skip buttons
+skipButtons.forEach(button => button.addEventListener('click', skipVideo));
+
+// Event listener for updating progress bar
